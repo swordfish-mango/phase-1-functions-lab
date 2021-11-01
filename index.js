5033c138fdbde09e07e1be37251417640eefa813
Marcus <marcus.shimotsu@gmail.com>
2021-11-01 12:22:36 -0500

    commit tree 78008450cd091b781f1ca327e0da91b56d261c2d

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return someValue > 42 ? someValue - 42 : 42 - someValue;
+}
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue)*264;
+}
+
+function distanceTravelledInFeet(startBlock,endBlock) {
+    return 264*(startBlock < endBlock ? endBlock - startBlock : startBlock - endBlock);
+}
+
+function calculatesFarePrice(startBlock,endBlock) {
+    const distance = distanceTravelledInFeet(startBlock,endBlock);
+    if (distance <= 400) {
+        return 0;
+    }
+    if (distance < 2000) {
+        return (distance - 400) * .02;
+    }
+    if (distance < 2500) {
+        return 25;
+    }
+    return "cannot travel that far";
+}
